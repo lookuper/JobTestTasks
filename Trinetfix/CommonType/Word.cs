4b70884977d58d526935f47853418cb8f2b84b72
Maksym Chernenko <maksym.chernenko@gmail.com>
2015-05-11 17:14:56 +0300

    add word/wordposition classes

--- Trinetfix/CommonType/Word.cs
+++ Trinetfix/CommonType/Word.cs
@@ -2,5 +2,15 @@
 {
     public class Word
     {
+        public string Location { get; private set; }
+        public string Text { get; private set; }
+        public WordPosition Position { get; private set; }
+
+        public Word(string location, string word, int row, int column)
+        {
+            Location = location;
+            Text = word;
+            Position = new WordPosition(row, column);
+        }
     }
 }
---
+++ Trinetfix/CommonType/WordPosition.cs
@@ -0,0 +1,14 @@
+﻿namespace CommonType
+{
+    public class WordPosition
+    {
+        public int Row { get; private set; }
+        public int Column { get; private set; }
+
+        public WordPosition(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+}
