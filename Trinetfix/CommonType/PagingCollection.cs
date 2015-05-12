d84fd5d880e6c36e7ee886ec99cdfba3ad6798a2
Maksym Chernenko <maksym.chernenko@gmail.com>
2015-05-12 21:14:20 +0300

    add generic PagingCollection

---
+++ Trinetfix/CommonType/PagingCollection.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonType
+{
+    public sealed class PagingCollection<T> : IEnumerable<T>
+    {
+        private int _pageSize = 100;
+        private IEnumerable<T> _collection;
+
+        public int PageSize
+        {
+            get { return _pageSize; }
+            set { if (value <= 0) throw new ArgumentException("value"); _pageSize = value; }
+        }
+
+        public int PageCount
+        {
+            get { return (int)Math.Ceiling(_collection.Count() / (decimal)PageSize); }
+        }
+
+        public PagingCollection(IEnumerable<T> source, int pageSize)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            _collection = source;
+            PageSize = pageSize;
+        }
+
+        public IEnumerable<T> GetDataFromPage(int pageNumber)
+        {
+            if (pageNumber <= 0 || pageNumber > PageCount)
+                return new T[] { };
+
+            int offset = (pageNumber - 1) * PageSize;
+            return _collection.Skip(offset).Take(PageSize).ToList();
+        }
+
+        public int GetCount(int pageNumber)
+        {
+            return GetDataFromPage(pageNumber).Count();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _collection.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
