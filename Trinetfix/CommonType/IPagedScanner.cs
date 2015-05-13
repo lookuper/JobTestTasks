69fd5200c4e738477df95868d0f9f93888072065
Maksym Chernenko <maksym.chernenko@gmail.com>
2015-05-13 23:47:49 +0300

    add IPagedScanner interface

---
+++ Trinetfix/CommonType/IPagedScanner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonType
+{
+    public interface IPagedScanner : IDisposable
+    {
+        /// <summary>
+        /// Get all scanned from all avaliable source and storages
+        /// </summary>
+        PagingCollection<ScanResult> GetScannedFiles();
+
+        /// <summary>
+        /// Get all words from file, spliting by regexp, default: "\w+[^\s]*\w+|\w"
+        /// </summary>
+        PagingCollection<Word> WordsInFile(string fileName, string regExp = null);
+
+        /// <summary>
+        /// Get all word positions from file
+        /// </summary>
+        PagingCollection<WordPosition> WordPositionsInFile(string fileName, string word);
+    }
+}
