b98b8724825a6fd45efd91f4a23487d297f405d3
Maksym Chernenko <maksym.chernenko@gmail.com>
2015-05-14 22:53:56 +0300

    add IStorage

---
+++ Trinetfix/CommonType/ISource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonType
+{
+    public interface ISource : IScanner
+    {
+        string Destination { get; }
+        string Filter { get; }
+
+        IEnumerable<ScanResult> Scan();
+    }
+}
