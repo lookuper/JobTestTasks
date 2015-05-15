6c75dab3bbdc2f6be9610699ee14a5c90b435d2b
Maksym Chernenko <maksym.chernenko@gmail.com>
2015-05-15 21:50:11 +0300

    add IStorage

---
+++ Trinetfix/CommonType/IStorage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonType
+{
+    public interface IStorage : IScanner
+    {
+        void Sync(IEnumerable<ScanResult> scanResult);
+    }
+}
