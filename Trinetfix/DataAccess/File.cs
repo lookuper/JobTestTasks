07883a7d54e7ffe3eea28b1aaf01c374da6744e2
Maksym Chernenko <maksym.chernenko@gmail.com>
2015-05-19 22:30:24 +0300

    add file

---
+++ Trinetfix/DataAccess/File.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class File
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; private set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        public DateTime CreatedDate { get; set; }
+        public virtual ICollection<Word> Words { get; set; }
+
+        public File() { }
+    }
+
+}
+
