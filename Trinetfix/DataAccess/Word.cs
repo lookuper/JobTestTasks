0ef88a69b21d36ec6637aaab418b20eea7e2a347
Maksym Chernenko <maksym.chernenko@gmail.com>
2015-05-20 22:49:53 +0300

    add Word class

---
+++ Trinetfix/DataAccess/Word.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class Word
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; private set; }
+        [Required]
+        public string Text { get; set; }
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        public int Row { get; set; }
+        [Required]
+        public int Column { get; set; }
+        [Required]
+        public int FileId { get; set; }
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
+        public Word() { }
+    }
+}
