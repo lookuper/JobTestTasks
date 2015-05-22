01984191124b30301abd9efb1a1fdfc0ea91474f
Maksym Chernenko <maksym.chernenko@gmail.com>
2015-05-22 21:40:00 +0300

    add UnitOfWork

---
+++ Trinetfix/DataAccess/UnitOfWork.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class UnitOfWork : IDisposable
+    {
+        private readonly DbContext _context = new TrinetfixContext();
+        private GenericRepository<File> _fileRepo;
+        private GenericRepository<Word> _wordRepo;
+        private bool _disposed = false;
+
+        public GenericRepository<File> FileRepository
+        {
+            get
+            {
+                _fileRepo = _fileRepo ?? new GenericRepository<File>(_context);
+                return _fileRepo;
+            }
+        }
+
+        public GenericRepository<Word> WordRepository
+        {
+            get
+            {
+                _wordRepo = _wordRepo ?? new GenericRepository<Word>(_context);
+                return _wordRepo;
+            }
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            _disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
