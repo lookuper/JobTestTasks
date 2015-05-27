44a30d6240c0cbe45fea465a4f18f005663ca6f8
Maksym Chernenko <maksym.chernenko@gmail.com>
2015-05-27 23:02:19 +0300

    add class programm to empty db initialization

---
+++ Trinetfix/DataAccess/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // required for manual db data cleanup
+            var contextForManualReCreation = new TrinetfixContext();
+            var filesRepo = new GenericRepository<File>(contextForManualReCreation);
+            var wordRepo = new GenericRepository<Word>(contextForManualReCreation);
+
+            var files = filesRepo.GetQueryable().ToList();
+            var words = wordRepo.GetQueryable().ToList();
+        }
+    }
+}
