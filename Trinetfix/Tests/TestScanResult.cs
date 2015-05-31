5d9b100011eaaf0b789a79b5d35dea80d50ec5af
Maksym Chernenko <maksym.chernenko@gmail.com>
2015-05-31 11:46:59 +0300

    add TestScanResult

--- Trinetfix/Tests/TestScanResult.cs
+++ Trinetfix/Tests/TestScanResult.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using CommonType;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Tests
 {
-    internal class TestScanResult : ScanResult
+    public class TestScanResult : ScanResult
     {
-        public DateTime Created { get; set; }
-        public string Location { get; set; }
-        public string Name { get; set; }
+        public TestScanResult() : base("test name", "test location")
+        {
+
+        }
+
+        protected TestScanResult(string name, string location) : base(name, location)
+        {
+        }
+
+        public override DateTime Created { get; set; }
+        public override long Size { get; set; }
+
+        public override string GetContent(Encoding encoding)
+        {
+            return @"I'm sure those are not the right words, said poor Alice";
+        }
+
+        public override IEnumerable<Word> GetWords(Encoding encoding = null, string regExp = null)
+        {
+            var words = SplitIntoWordsWithIndex(GetContent(null), regExp)
+                    .Select(w => new Word(Location, w.Item1, 1, w.Item2))
+                    .ToList();
+
+            return words;
+        }
     }
 }
